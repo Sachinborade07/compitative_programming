6051a0e46b8652fc1d4ac31ad698e146ffc670f4
SACHIN BORADE <123536691+Sachinborade07@users.noreply.github.com>
2024-07-19 22:30:34 +0530

    Update first_last_occrence.cpp

--- Searching_Sorting/first_last_occrence.cpp
+++ Searching_Sorting/first_last_occrence.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+void findFirstAndLast(int arr[], int n, int x)
+{
+	int first, last;
+
+
+    // this is our main logic
+	// the first occurence
+	first = lower_bound(arr, arr + n, x) - arr;
+	// the last occurence
+	last = upper_bound(arr, arr + n, x) - arr - 1;
+
+
+	if (first == n) {
+		first = -1;
+		last = -1;
+	}
+	cout << "First Occurrence = " << first << "\nLast Occurrence = " << last;
+}
+
+int main()
+{
+	int arr[] = { 1, 2, 2, 2, 2, 3, 4, 7, 8, 8 };
+	int n = sizeof(arr) / sizeof(int);
+	int x = 2;
+	findFirstAndLast(arr, n, x);
+	return 0;
+}
