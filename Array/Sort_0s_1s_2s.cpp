71635708a17a0d81fe2001ac65030434c9c77eb7
SACHIN BORADE <123536691+Sachinborade07@users.noreply.github.com>
2024-07-20 18:24:16 +0530

    Create Sort_0s_1s_2s.cpp

---
+++ Array/Sort_0s_1s_2s.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+    public:
+    void sort012(int a[],int n)
+    {
+      // code here
+      sort(a,a+n);
+    }
+}
+
+// this code is from geeks for geeks 
+https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
