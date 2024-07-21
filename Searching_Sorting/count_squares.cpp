a0d6dcf5b1bfed7ecdb22831e5b8bf1e9a9fe07b
SACHIN BORADE <123536691+Sachinborade07@users.noreply.github.com>
2024-07-21 23:12:03 +0530

    Create count_squares.cpp

---
+++ Searching_Sorting/count_squares.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    
+    int countSquares(int N) {
+        // code here
+        return static_cast<int>(sqrt(N-1));
+    }
+};
+// This is the code from geeks for geeks 
+https://www.geeksforgeeks.org/problems/count-squares3649/1
