c14b3332913fa87810c2b4312796fc1bcdfb38a4
SACHIN BORADE <123536691+Sachinborade07@users.noreply.github.com>
2024-07-21 23:44:19 +0530

    Create Segregate_0s_and_1s.cpp

---
+++ Array/Segregate_0s_and_1s.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        // code here
+        
+        sort(arr.begin(),arr.end());
+    }
+};
+
+// This code is from geeks for geeks 
+https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1
